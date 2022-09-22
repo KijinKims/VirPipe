fbc5d42d0a62f6f25151b5b299d6294e47134b52
Kijin Kim <skkujin@gmail.com>
2022-09-22 17:03:23 +0900

    rename end_to_end to all

--- src/virpipe/arguments_parser.py
+++ src/virpipe/arguments_parser.py
@@ -188,10 +188,10 @@ class Parser:
         report_blast_parser = report_subparsers.add_parser('blast', parents=[shared_parser, general_input_parser], argument_default=argparse.SUPPRESS)
         report_blast_parser.add_argument('--taxonomizr_db', nargs='?', type=lambda x: parser_path_check(report_blast_parser, valid_file, x))
 
-        end_to_end_parser = subparsers.add_parser('end_to_end', parents=[shared_parser, platform_parser, input_parser], argument_default=argparse.SUPPRESS)
-        end_to_end_parser.add_argument('--host_genome', nargs='*', type=lambda x: parser_path_check(end_to_end_parser, valid_fasta, x))
-        end_to_end_parser.add_argument('--include_zoonotic_rank', action='store_true', default=True)
-        end_to_end_parser.add_argument('--assembly_tool', nargs='?', choices=['spades', 'megahit', 'canu', 'flye'])
+        all_parser = subparsers.add_parser('all', parents=[shared_parser, platform_parser, input_parser], argument_default=argparse.SUPPRESS)
+        all_parser.add_argument('--host_genome', nargs='*', type=lambda x: parser_path_check(all_parser, valid_fasta, x))
+        all_parser.add_argument('--include_zoonotic_rank', action='store_true', default=True)
+        all_parser.add_argument('--assembly_tool', nargs='?', choices=['spades', 'megahit', 'canu', 'flye'])
 
         consensus_parser = subparsers.add_parser('consensus', parents=[shared_parser, platform_parser, input_parser], argument_default=argparse.SUPPRESS)
         consensus_parser.add_argument('--ref', nargs='*', type=lambda x: parser_path_check(consensus_parser, valid_fasta, x))
