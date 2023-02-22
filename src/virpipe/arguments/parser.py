8a8868c7adc3b2cb4ca21064b9f268140bd7f7d6
Kijin Kim <skkujin@gmail.com>
2023-02-22 12:49:55 +0900

    fix boolean default

--- src/virpipe/arguments/parser.py
+++ src/virpipe/arguments/parser.py
@@ -45,13 +45,13 @@ class Parser:
         qc_parser.add_argument('--nanopore-min-read-length', nargs='?', type=check_positive_int, default=200)
 
         preprocess_parser = subparsers.add_parser('preprocess', parents=[common_parser, platform_parser, fastq_input_parser], argument_default=argparse.SUPPRESS)
-        preprocess_parser.add_argument('--save-preprocessed', action='store_true', default=True)
+        preprocess_parser.add_argument('--save-preprocessed', action='store_true', default=False)
         preprocess_parser.add_argument('--illumina-min-read-quality', nargs='?', type=check_positive_int, default=8)
         preprocess_parser.add_argument('--nanopore-min-read-quality', nargs='?', type=check_positive_int, default=8)
         preprocess_parser.add_argument('--nanopore-min-read-length', nargs='?', type=check_positive_int, default=200)
         
         remove_host_parser = subparsers.add_parser('remove-host', parents=[common_parser, platform_parser, fastq_input_parser], argument_default=argparse.SUPPRESS)
-        remove_host_parser.add_argument('--save-host-removed', action='store_true', default=True)
+        remove_host_parser.add_argument('--save-host-removed', action='store_true', default=False)
         remove_host_parser.add_argument('--host-genome', nargs='?', required=True)
 
         map_parser = subparsers.add_parser('map', parents=[common_parser, platform_parser, fastq_input_parser], argument_default=argparse.SUPPRESS)
@@ -81,11 +81,11 @@ class Parser:
         zoonotic_rank_parser = subparsers.add_parser('zoonotic-rank', parents=[common_parser, fasta_input_parser], argument_default=argparse.SUPPRESS)
 
         all_parser = subparsers.add_parser('all', parents=[common_parser, platform_parser, fastq_input_parser], argument_default=argparse.SUPPRESS)
-        all_parser.add_argument('--save-preprocessed', action='store_true', default=True)
+        all_parser.add_argument('--save-preprocessed', action='store_true', default=False)
         all_parser.add_argument('--illumina-min-read-quality', nargs='?', type=check_positive_int, default=8)
         all_parser.add_argument('--nanopore-min-read-quality', nargs='?', type=check_positive_int, default=8)
         all_parser.add_argument('--nanopore-min-read-length', nargs='?', type=check_positive_int, default=200)
-        all_parser.add_argument('--save-host-removed', action='store_true', default=True)
+        all_parser.add_argument('--save-host-removed', action='store_true', default=False)
         all_parser.add_argument('--host-genome', nargs='?')
         all_parser.add_argument('--save-map-bam', action='store_true', default=False)
         all_parser.add_argument('--ref', nargs='*')
