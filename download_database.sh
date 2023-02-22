4e1ee46789a0a727adffc855bfae38497f937dd5
Kijin Kim <skkujin@gmail.com>
2023-02-22 12:04:44 +0900

    update download link

--- download_database.sh
+++ download_database.sh
@@ -1,15 +1,15 @@
 # download BLAST database
-curl --output blast_refseq_viral_230213.tar.gz https://zenodo.org/api/files/402dcb71-c95b-40fd-9ef4-6d3906594c61/blast_refseq_viral_230213.tar.gz
-mkdir -p blast_refseq_viral && tar -xzvf blast_refseq_viral_230213.tar.gz -C blast_refseq_viral
+curl --output blast_ref_viruses_rep_genomes.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/db/v5/v5/ref_viruses_rep_genomes.tar.gz
+mkdir -p blast_ref_viruses_rep_genomes && tar -xzvf blast_ref_viruses_rep_genomes.tar.gz -C blast_ref_viruses_rep_genomes
 
 # download Kraken2 database
 curl --output k2_viral_20221209.tar.gz https://genome-idx.s3.amazonaws.com/kraken/k2_viral_20221209.tar.gz
 mkdir -p k2_viral && tar -zxvf k2_viral_20221209.tar.gz -C k2_viral
 
 # download centrifuge database
-curl --output centrifuge_viral_230213.tar.gz https://zenodo.org/api/files/402dcb71-c95b-40fd-9ef4-6d3906594c61
+curl --output centrifuge_viral_230213.tar.gz https://zenodo.org/api/files/814e2a69-a24c-4810-80a1-06fe3ce3bec9/centrifuge_viral_230213.tar.gz
 mkdir -p centrifuge_viral && tar -xzvf blast_refseq_viral_230213.tar.gz -C centrifuge_refseq_viral
 
 # download taxonomizr database
-curl --output accessionTaxa.sql.gz https://zenodo.org/api/files/402dcb71-c95b-40fd-9ef4-6d3906594c61/accessionTaxa.sql.gz 
+curl --output accessionTaxa.sql.gz https://zenodo.org/api/files/814e2a69-a24c-4810-80a1-06fe3ce3bec9/accessionTaxa.sql.gz
 gunzip accessionTaxa.sql.gz
