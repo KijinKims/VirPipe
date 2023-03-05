8f20cf785cf1e6ac79e1e1f2bbd29fa07bca971a
Kijin Kim <skkujin@gmail.com>
2023-03-05 22:42:43 +0900

    donwload database script fixed

--- download_database.sh
+++ download_database.sh
@@ -9,9 +9,9 @@ curl --output k2_viral_20221209.tar.gz https://genome-idx.s3.amazonaws.com/krake
 mkdir -p databases/k2_viral && tar -zxvf k2_viral_20221209.tar.gz -C databases/k2_viral
 
 # download centrifuge database
-curl --output centrifuge_viral_230213.tar.gz https://zenodo.org/api/files/814e2a69-a24c-4810-80a1-06fe3ce3bec9/centrifuge_viral_230213.tar.gz
+curl --output centrifuge_viral_230213.tar.gz https://zenodo.org/record/7662919/files/centrifuge_viral_230213.tar.gz?download=1
 mkdir -p databases/centrifuge_db && tar -xzvf blast_refseq_viral_230213.tar.gz -C databases/centrifuge_db
 
 # download taxonomizr database
-curl --output databases/accessionTaxa.sql.gz https://zenodo.org/api/files/814e2a69-a24c-4810-80a1-06fe3ce3bec9/accessionTaxa.sql.gz
+curl --output databases/accessionTaxa.sql.gz https://zenodo.org/record/7662919/files/accessionTaxa.sql.gz?download=1
 gunzip databases/accessionTaxa.sql.gz
